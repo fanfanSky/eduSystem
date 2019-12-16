31c620bcd21731a6e81f8776077a5fb08e5fe122
J-Fan-Fan <1354124729@qq.com>
2019-12-16 20:58:36 +0800

    首页模块 - 左侧导航折叠

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,13 +1,119 @@
 <template>
-    <div>我是一个帅帅滴首页呀!</div>
+  <!-- 布局容器 -->
+  <el-container class="index-container">
+    <el-header class="my-header">
+      <div class="left">
+        <!-- 图标 -->
+        <i @click="isCollapse = !isCollapse" class="el-icon-s-fold icon"></i>
+        <img class="logo" src="../../assets/index_logo.png" alt />
+        <span class="title">黑马面面</span>
+      </div>
+      <div class="right">
+        <img class="user-icon" src="../../assets/小可爱.jpg" alt />
+        <span class="user-name">李达,您好</span>
+        <el-button type="primary" size="small">退出</el-button>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="auto" class="my-aside">
+        <!-- 导航菜单 -->
+        <el-menu
+          router
+          default-active="/index/user"
+          :collapse="isCollapse"
+          class="el-menu-vertical-demo"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <el-menu-item index="/index/user">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <el-menu-item index="/index/subject">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main class="my-main">
+        <!-- 出口 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+      name: "index",
+  data() {
+    return {
+      // 是否折叠
+      isCollapse: false
+    };
+  }
+};
 </script>
 
-<style>
-
+<style lang="less">
+.index-container {
+  height: 100%;
+  .my-header {
+    // background-color: skyblue;
+    display: flex;
+    justify-content: space-between;
+    .left {
+      display: flex;
+      align-items: center;
+      .icon {
+        font-size: 24px;
+        margin-right: 22px;
+      }
+      .logo {
+        width: 33px;
+        height: 28px;
+        margin-right: 11px;
+      }
+      .title {
+        font-size: 22px;
+        color: #49a1ff;
+      }
+    }
+    .right {
+      display: flex;
+      align-items: center;
+      .user-icon {
+        border-radius: 50%;
+        width: 43px;
+        height: 43px;
+        margin-right: 9px;
+      }
+      .user-name {
+        font-size: 14px;
+        margin-right: 38px;
+      }
+    }
+  }
+  .my-aside {
+    // background-color: green;
+    // 导航样式
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+      width: 200px;
+      min-height: 400px;
+    }
+  }
+  .my-main {
+    background-color: purple;
+  }
+}
 </style>
