ff79252c6bc3826ae947efc853a11dbd85da91b7
J-Fan-Fan <1354124729@qq.com>
2019-12-17 23:58:45 +0800

    登录验证模块 - Vuex整合

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+// store.js
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// use一下
+Vue.use(Vuex)
+
+// 创建仓库
+const store = new Vuex.Store({
+    state: {
+        // 用户信息
+        userInfo: {}
+    }
+})
+// 暴露出去
+export default store
