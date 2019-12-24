d03e72af6c080b559147cf3c773d8e160b96c34b
J-Fan-Fan <1354124729@qq.com>
2019-12-24 15:22:25 +0800

    权限模块 - 自定义指令控制元素

--- src/directives/directives.js
+++ src/directives/directives.js
@@ -6,8 +6,8 @@ import store from "../store/store.js";
 // 只需要保证执行即可
 Vue.directive("power", {
     inserted: function (el, binding) {
-        window.console.log(el);
-        window.console.log(binding.value);
+        // window.console.log(el);
+        // window.console.log(binding.value);
         if (binding.value.includes(store.state.userInfo.role)) {
             // 配置的规则中 存在当前这个角色，就移除他
             el.parentNode.removeChild(el);
